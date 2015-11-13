b95d35663ed094dccfaf1f35d2059ae235ac8c5d
zhaoyulin <842215935@qq.com>
2015-11-13 14:06:25 +0800

    test

--- src/git_demo/Demo1.java
+++ src/git_demo/Demo1.java
@@ -1,7 +1,12 @@
 package git_demo;
 
 public class Demo1 {
+	public void test(){
+		System.out.println("test111111");
+	}
 	public static void main(String[] args) {
+		Demo1 de = new Demo1();
+		de.test();
 		System.out.println("HelloWorld!");
 	}
 }
